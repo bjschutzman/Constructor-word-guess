11549261fb910400b749c4d4a571376143cbc78e
Brian Schutzman <bjschutzman@gmail.com>
2019-05-18 09:47:11 -0500

    not comlete

--- Letter.js
+++ Letter.js
@@ -1,7 +1,47 @@
 // Letter.js: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
 
 
-// A string value to store the underlying character for the letter
-// A boolean value that stores whether that letter has been guessed yet
-// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
+
+var Letter = function(letter){
+    // A string value to store the underlying character for the letter
+    this.char = letter; 
+    // A boolean value that stores whether that letter has been guessed yet
+    this.guessed = false;
+   
+   
+
+
+
+// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+        Letter.prototype.showLetter = function(){
+            if(this.guessed == true){
+                displayChar = this.char;
+            }
+            else{
+                displayChar= "_"
+            }
+        console.log(displayChar);
+        }
+
+
+
+        // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+        Letter.prototype.displayLetter = function(userGuess){
+                if(this.char=== userGuess){
+                    this.guessed = true;
+                }
+                
+            }
+            
+        }
+
+// var a = new Letter("a")
+// console.log(a.char)
+// console.log(a.guessed);
+// a.showLetter();
+
+module.exports = Letter;
+
+
--- index.js
+++ index.js
@@ -4,15 +4,66 @@
 // Randomly selects a word and uses the Word constructor to store it
 // Prompts the user for each guess and keeps track of the user's remaining guesses
 var wordArray = ["fish", "camel", "elephant"]
-var chosenWord = wordArray[Math.floor(Math.random() * wordArray.length)]
-var userInput = "";
+var chosenWord = ""
+var lettersInWord = [];
+var numBlanks = 0
 var blanksAndSucess = ""
+var wrongGuess = []
 var guessesLeft = 9
-// var lettersInWord = chosenWord[i].split(" ")
-// var letterBlank = lettersInWord.push("_");
 
-console.log(wordArray);
-console.log(chosenWord);
+
+// start game function sets up everything needed to start
+    function startGame(){
+    guessesLeft = 9
+
+    // Randomly selects a word and uses the Word constructor to store it
+    chosenWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    console.log(chosenWord);
+    // splits chosen word into indvidual letters
+    lettersInWord = chosenWord.split("");
+    // counts how mant letters are in lettersInWord
+    numBlanks = lettersInWord.length;
+    console.log(numBlanks);
+
+    blanksAndSucess = [];
+    wrongGuess =[];
+
+    for(var i = 0; i < numBlanks; i++){
+        blanksAndSucess.push("_")
+    }
+    console.log(blanksAndSucess);
+}
+
+
+
+function letterCheck(letter){
+    // sets boolean so that letterInWord is false until told otherwise
+    var letterInword = false
+
+    // loop to see if letter is in the selected word
+    for (var i = 0; i < numBlanks; i++){
+        if (chosenWord === numblanks){
+            letterInword === true
+        }
+    }
+
+    // if the letter is in the word this will loop through and find where that letter is an push the letter to blanksAndSuccesses
+    if (letterInword){
+        for (var j = 0; j < numblanks; j++){
+            if(chosenWord[j] ===letter){
+                blanksAndSucess[j] = letter;
+            }
+            console.log(blanksAndSucess)
+        }
+    }
+    else{
+        wrongGuess.push(letter);
+        guessesLeft--
+    }
+}
+
+
+
 
 // var lettersInWord = chosenWord[i].split(" ")
 // var letterBlank = lettersInWord.push("_");
